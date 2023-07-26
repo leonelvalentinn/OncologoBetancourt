b1e9dea19cc48ecebd2d179f5cb7a848ec6fec7c
leonelvalentinn <leonelvalentinn@gmail.com>
2023-07-25 23:09:09 -0600

    Pagina full en celular

--- js/script.js
+++ js/script.js
@@ -1,41 +1,14 @@
-//Script para el slider
-const slider = document.querySelector("#slider");
-let sliderItem = document.querySelectorAll(".slider-item");
-let sliderItemLast = sliderItem[sliderItem.length-1];
-
-const btnLeft = document.querySelector("#btn-left");
-const btnRight = document.querySelector("#btn-right");
-
-slider.insertAdjacentElement('afterbegin', sliderItemLast);
-
-function Next() {
-    let sliderItemFirst = document.querySelectorAll(".slider-item")[0];
-    slider.style.marginLeft = "-200%";
-    
-    slider.style.transition = "all 2s";
-    setTimeout(function() {
-        slider.style.transition = "none";
-        slider.insertAdjacentElement('beforeend', sliderItemFirst);
-        slider.style.marginLeft = "-100%";
-    }, 50);
+//Script menu
+function openNav() {
+  document.getElementById("mobile-menu").style.width = "100%";
+  document.getElementById("closeMenu").style.animation = "menuMobile 0.5s linear";
+  document.getElementById("openMenu").style.display = "none";
+  document.getElementById("closeMenu").style.display = "flex";
 }
 
-function Prev() {
-    let sliderItem = document.querySelectorAll(".slider-item");
-    let sliderItemLast = sliderItem[sliderItem.length-1];
-    slider.style.marginLeft = "0";
-    slider.style.transition = "all 2s";
-    setTimeout(function() {
-        slider.style.transition = "none";
-        slider.insertAdjacentElement('afterbegin', sliderItemLast);
-        slider.style.marginLeft = "-100%";
-    }, 50);
+function closeNav() {
+  document.getElementById("mobile-menu").style.width = "0%";
+  document.getElementById("openMenu").style.animation = "menuMobile 0.5s linear";
+  document.getElementById("openMenu").style.display = "flex";
+  document.getElementById("closeMenu").style.display = "none";
 }
-
-btnRight.addEventListener('click', function(){
-    Next();
-})
-
-btnLeft.addEventListener('click', function(){
-    Prev();
-})
