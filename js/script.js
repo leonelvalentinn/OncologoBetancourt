acd9aa2b229c9a87ffb4e6833a5fe4b462a06639
leonelvalentinn <leonelvalentinn@gmail.com>
2023-08-02 17:03:25 -0600

    Full index

--- js/script.js
+++ js/script.js
@@ -12,3 +12,49 @@ function closeNav() {
   document.getElementById("openMenu").style.display = "flex";
   document.getElementById("closeMenu").style.display = "none";
 }
+
+//script menu collapse
+function openMenu() {
+  if (document.getElementById("acordeon").style.display == "block") {
+    document.getElementById("acordeon").style.display = "none";
+    document.getElementById("acordeon").style.animation = "collapseD 0.5s linear reverse"
+    document.getElementById("arrow").style.animation = "menuDireccion 0.5s linear reverse forwards";
+  } else {
+    document.getElementById("acordeon").style.display = "block";
+    document.getElementById("acordeon").style.animation = "collapseD 0.5s linear"
+    document.getElementById("arrow").style.animation = "menuDireccion 0.5s linear forwards";
+  }
+}
+function openMenu2() {
+  if (document.getElementById("acordeon-2").style.display == "block") {
+    document.getElementById("acordeon-2").style.display = "none";
+    document.getElementById("acordeon-2").style.animation = "collapseD 0.5s linear reverse"
+    document.getElementById("arrow-2").style.animation = "menuDireccion 0.5s linear reverse forwards";
+  } else {
+    document.getElementById("acordeon-2").style.display = "block";
+    document.getElementById("acordeon-2").style.animation = "collapseD 0.5s linear"
+    document.getElementById("arrow-2").style.animation = "menuDireccion 0.5s linear forwards";
+  }
+}
+function openMenu3() {
+  if (document.getElementById("acordeon-3").style.display == "block") {
+    document.getElementById("acordeon-3").style.display = "none";
+    document.getElementById("acordeon-3").style.animation = "collapseD 0.5s linear reverse"
+    document.getElementById("arrow-3").style.animation = "menuDireccion 0.5s linear reverse forwards";
+  } else {
+    document.getElementById("acordeon-3").style.display = "block";
+    document.getElementById("acordeon-3").style.animation = "collapseD 0.5s linear"
+    document.getElementById("arrow-3").style.animation = "menuDireccion 0.5s linear forwards";
+  }
+}
+function openMenu4() {
+  if (document.getElementById("acordeon-4").style.display == "block") {
+    document.getElementById("acordeon-4").style.display = "none";
+    document.getElementById("acordeon-4").style.animation = "collapseD 0.5s linear reverse"
+    document.getElementById("arrow-4").style.animation = "menuDireccion 0.5s linear reverse forwards";
+  } else {
+    document.getElementById("acordeon-4").style.display = "block";
+    document.getElementById("acordeon-4").style.animation = "collapseD 0.5s linear"
+    document.getElementById("arrow-4").style.animation = "menuDireccion 0.5s linear forwards";
+  }
+}
